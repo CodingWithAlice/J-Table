25d038dc8b392afd2c54ccbc06ddcc13d0f91230
haoling <haoling@corp.netease.com>
2024-03-08 16:03:33 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='title'>
+          一个J人的2024年
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
