d48e6fe8ab77efbbeab01c6c6b6385c6d3a0e273
haoling <haoling@corp.netease.com>
2024-05-07 20:13:04 +0800

    add:复盘表结构搭建，具体指标需设计后补充

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
 
+import { Table } from 'antd';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className='title'>
-          一个J人的2024年
-        </p>
-      </header>
-    </div>
-  );
+    const dataSource = [
+        {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+        },
+        {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+        },
+    ];
+
+    const columns = [
+        {
+            title: '分类',
+            dataIndex: 'type',
+            key: 'type',
+        },
+        {
+            title: '目标',
+            dataIndex: 'target',
+            key: 'target',
+        },
+        {
+            title: '量化',
+            dataIndex: 'execution',
+            key: 'execution',
+        },
+        {
+            title: '每周',
+            dataIndex: 'weekday',
+            key: 'weekday',
+        },
+        {
+            title: '完成率',
+            dataIndex: 'portion',
+            key: 'portion',
+        },
+        {
+            title: '解释一下吧',
+            dataIndex: 'explain',
+            key: 'explain',
+        },
+    ];
+
+    return (
+        <div className="App">
+            {/* 大标题 */}
+            <h1 className='title'>
+                J人热爱统计的一生
+            </h1>
+
+            {/* 可量化目标 */}
+            <Table
+                dataSource={dataSource}
+                columns={columns}
+                pagination={false}
+                title={() =>
+                    // Week 信息
+                    <div className='week'>Week 9 复盘</div>
+                }
+                className='table' />;
+            {/* 不可量化目标 */}
+
+        </div>
+    );
 }
 
 export default App;
