df0b0f2aa687e75aececa557b370ec5777d167c4
haoling <haoling@corp.netease.com>
2024-05-08 20:00:23 +0800

    add:手写表单-> 需改为 children

--- src/App.js
+++ src/App.js
@@ -1,53 +1,70 @@
-
-import { Table } from 'antd';
+import { Space, Table, Tag } from 'antd';
 import './App.css';
 
+const { Column, ColumnGroup } = Table;
+
 function App() {
-    const dataSource = [
+    // 固定表头
+    const headers = [
+        {
+            key: 'category',
+            dataIndex: 'category',
+            title: '分类'
+        },
+        {
+            key: 'target',
+            dataIndex: 'target',
+            title: '目标'
+        },
+        {
+            key: 'quantify',
+            dataIndex: 'quantify',
+            title: '量化'
+        },
+        {
+            type: 'group',
+            key: 'weekday',
+            dataIndex: 'weekday',
+            title: '周完成情况',
+            dataSource: Array.from(Array(7).keys(), (x) => x + 1).map((weekday) => {
+                return {
+                    title: `周${weekday}`,
+                    key: `${weekday}`,
+                    dataIndex: `${weekday}`,
+                }
+            })
+        },
+        {
+            key: 'excuse',
+            dataIndex: 'excuse',
+            title: '解释一下吧'
+        },
+    ]
+    // 模拟数据
+    const data = [
         {
             key: '1',
-            name: '胡彦斌',
+            firstName: 'John',
+            lastName: 'Brown',
             age: 32,
-            address: '西湖区湖底公园1号',
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
         },
         {
             key: '2',
-            name: '胡彦祖',
+            firstName: 'Jim',
+            lastName: 'Green',
             age: 42,
-            address: '西湖区湖底公园1号',
-        },
-    ];
-
-    const columns = [
-        {
-            title: '分类',
-            dataIndex: 'type',
-            key: 'type',
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
         },
         {
-            title: '目标',
-            dataIndex: 'target',
-            key: 'target',
-        },
-        {
-            title: '量化',
-            dataIndex: 'execution',
-            key: 'execution',
-        },
-        {
-            title: '每周',
-            dataIndex: 'weekday',
-            key: 'weekday',
-        },
-        {
-            title: '完成率',
-            dataIndex: 'portion',
-            key: 'portion',
-        },
-        {
-            title: '解释一下吧',
-            dataIndex: 'explain',
-            key: 'explain',
+            key: '3',
+            firstName: 'Joe',
+            lastName: 'Black',
+            age: 32,
+            address: 'Sydney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
         },
     ];
 
@@ -60,14 +77,26 @@ function App() {
 
             {/* 可量化目标 */}
             <Table
-                dataSource={dataSource}
-                columns={columns}
+                dataSource={data}
                 pagination={false}
                 title={() =>
                     // Week 信息
                     <div className='week'>Week 9 复盘</div>
                 }
-                className='table' />;
+                className='table' >
+                {/* 分类 + 目标 + 量化 */}
+                {headers.map((header) => {
+                    if (header?.type !== 'group') {
+                        return <Column title={header.title} dataIndex={header.dataIndex} key={header.key} />
+                    } else {
+                        return <ColumnGroup title={header.title}>
+                            {header?.dataSource ? header?.dataSource.map((data) => <Column title={data.title} dataIndex={data.dataIndex} key={data.key} />) : <></>}
+
+
+                        </ColumnGroup>
+                    }
+                })}
+            </Table>
             {/* 不可量化目标 */}
 
         </div>
