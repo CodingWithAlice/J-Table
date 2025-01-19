54d083315c942323d8037571ca4f949d06f207ab
CodingWithAlice <2269131254@qq.com>
2025-01-19 17:04:24 +0800

    style: 优化代码结构

--- package/web/src/components/EdibleTable.js
+++ package/web/src/components/EdibleTable.js
@@ -4,10 +4,10 @@ import {
 } from '@ant-design/pro-components';
 import { Tag } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { Category, CategoryColor } from './utils/utils';
-import { getWeek } from './utils/getWeeks';
-import { MainApi } from './apis/main';
-import './App.css';
+import { Category, CategoryColor } from '../utils/utils';
+import { getWeek } from '../utils/getWeeks';
+import { MainApi } from '../apis/main';
+import '../App.css';
 
 const waitTime = (time = 100) => {
     return new Promise((resolve) => {
---
+++ package/web/src/components/LtnTable.js
@@ -0,0 +1,3 @@
+export default function LtnTable() {
+    return <div>Table</div>
+}
