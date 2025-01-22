9b135c4f284ab8efd885efe2df12ad52a6d3ea78
CodingWithAlice <2269131254@qq.com>
2025-01-22 17:13:26 +0800

    delete: weekTable 移动到另一个项目

--- package/web/src/App.js
+++ package/web/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LtnTable from './components/LtnTable';
-import Main from './components/mainApp';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/ltn" element={<LtnTable />} />
+                <Route path="/" element={<LtnTable />} />
             </Routes>
         </Router >
     );
