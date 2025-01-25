fe2ddb370540da8abb49b55feaa0fb6cbfa41bef
CodingWithAlice <2269131254@qq.com>
2025-01-25 17:02:42 +0800

    add: LTN周期题目 easy copy

--- package/web/src/components/LtnTable.js
+++ package/web/src/components/LtnTable.js
@@ -6,11 +6,27 @@ import TimeModal from "./TimeModal";
 import AddLtn from "./AddLtn";
 import { message } from "antd";
 
+const easyCopy = (data) => {
+    const totalArr = Object.keys(data).reduce((pre, cur) => {
+        const count = data[cur].length;
+        pre.push(count);
+        return pre;
+    }, [])
+    console.log('过滤后的数据data', totalArr, Object.keys(data).map(it => {
+        const ltns = data[it];
+        const idDesc = ltns.length ? `LTN${it}
+` : '';
+        return idDesc + ltns.map(ltn => `${ltn.title}
+`).join('');
+    }).join(''));
+}
+
 export default function LtnTable() {
     let [ltns, setLtns] = useState([]);
     const init = (params) => {
         LtnApi.list(params).then((data) => {
             setLtns(data);
+            easyCopy(data);
             message.success('刷新成功');
         });
     }
