45ceaa1e6922369577919628526ecf3d7adcc89b
CodingWithAlice <2269131254@qq.com>
2025-02-17 10:29:39 +0800

    feat: 每个盒子按时间排序展示

--- package/web/src/components/LtnList.js
+++ package/web/src/components/LtnList.js
@@ -7,8 +7,11 @@ export default function LtnList({ list, boxId, fresh }) {
     const getNextTime = (solveTime) => {
         return dayjs(solveTime || '2025-01-20').add(boxId * 7, 'day').format('YYYY-MM-DD');
     }
+    const sortList = list.sort((a, b) => {;
+        return dayjs(a.solveTime).isBefore(dayjs(b.solveTime)) ? -1 : 1;
+    })
     return <div className="ltn-list">
-        {list.map((it, index) => <div key={it.id} className="ltn-item">
+        {sortList.map((it, index) => <div key={it.id} className="ltn-item">
             {/* 序号 */}
             <Avatar className="ltn-avatar" size='small'>{index + 1}</Avatar>
             {/* 默认单行展示 - 过长通过悬浮展示 */}
