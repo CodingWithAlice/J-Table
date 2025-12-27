33dcdfb69aef2587e9e09d25c93a9a43f57e63e2
CodingWithAlice <2269131254@qq.com>
2025-12-27 21:32:09 +0800

    feat: 过滤当前最小日期做题后刷新弹窗

--- package/web/src/components/FilterBtn.tsx
+++ package/web/src/components/FilterBtn.tsx
@@ -21,34 +21,38 @@ export default function FilterBtn() {
     };
 
     // 获取最小日期当天的数据
-    useEffect(() => {
-        if (isModalOpen) {
-            // 传递 useMinDate 标识，后端会返回最小日期当天的数据
-            LtnApi.list({ useMinDate: true }).then((data) => {
-                setLtns(data);
-                // 从所有数据中计算最小日期
-                let minDateValue: Date | null = null;
-                Object.keys(data).forEach((boxId) => {
-                    const boxData = data[boxId as LtnsType];
-                    if (Array.isArray(boxData)) {
-                        boxData.forEach((ltn: LtnDTO) => {
-                            if (ltn.solveTime) {
-                                const nextTime = new Date(
-                                    new Date(ltn.solveTime).getTime() +
-                                    ltn.customDuration * 24 * 60 * 60 * 1000
-                                );
-                                if (!minDateValue || nextTime < minDateValue) {
-                                    minDateValue = nextTime;
-                                }
+    const initMinDateData = () => {
+        // 传递 useMinDate 标识，后端会返回最小日期当天的数据
+        LtnApi.list({ useMinDate: true }).then((data) => {
+            setLtns(data);
+            // 从所有数据中计算最小日期
+            let minDateValue: Date | null = null;
+            Object.keys(data).forEach((boxId) => {
+                const boxData = data[boxId as LtnsType];
+                if (Array.isArray(boxData)) {
+                    boxData.forEach((ltn: LtnDTO) => {
+                        if (ltn.solveTime) {
+                            const nextTime = new Date(
+                                new Date(ltn.solveTime).getTime() +
+                                ltn.customDuration * 24 * 60 * 60 * 1000
+                            );
+                            if (!minDateValue || nextTime < minDateValue) {
+                                minDateValue = nextTime;
                             }
-                        });
-                    }
-                });
-                if (minDateValue !== null) {
-                    const dateStr = (minDateValue as Date).toISOString().split('T')[0];
-                    setMinDate(dateStr);
+                        }
+                    });
                 }
             });
+            if (minDateValue !== null) {
+                const dateStr = (minDateValue as Date).toISOString().split('T')[0];
+                setMinDate(dateStr);
+            }
+        });
+    };
+
+    useEffect(() => {
+        if (isModalOpen) {
+            initMinDateData();
         }
     }, [isModalOpen]);
 
@@ -74,7 +78,7 @@ export default function FilterBtn() {
                 {Object.keys(ltns).map((ltnType: LtnsType) => <div key={ltnType}>
                     {!!ltns[ltnType].length && <div key={ltnType}>
                         <h2 className="ltn-box">BOX{ltnType}</h2>
-                        <LtnList list={ltns[ltnType]} boxId={+ltnType} />
+                        <LtnList list={ltns[ltnType]} boxId={+ltnType} fresh={initMinDateData} />
                     </div>}
                 </div>)}
             </div>
